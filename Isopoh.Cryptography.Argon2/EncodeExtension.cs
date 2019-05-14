5a50dc27cbdafe41664d08a50350a7733d625925
Michael Heyman <Michael.Heyman@jhuapl.edu>
2019-05-14 19:42:33 -0400

    fix: no '=' padding in base-64 encoding as per Argon2 spec

--- Isopoh.Cryptography.Argon2/EncodeExtension.cs
+++ Isopoh.Cryptography.Argon2/EncodeExtension.cs
@@ -7,6 +7,7 @@
 namespace Isopoh.Cryptography.Argon2
 {
     using System;
+    using System.Globalization;
     using System.Text;
 
     /// <summary>
@@ -14,6 +15,19 @@ namespace Isopoh.Cryptography.Argon2
     /// </summary>
     public static class EncodeExtension
     {
+        private static readonly int[] B64Extra = { 0, 2, 3 };
+        private static readonly char[] b64Chars =
+        {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+            'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+            'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+            'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9', '+', '/',
+        };
+
+
         /// <summary>
         /// Encodes an Argon2 instance into a string.
         /// </summary>
@@ -81,7 +95,7 @@ namespace Isopoh.Cryptography.Argon2
             if (config.AssociatedData != null && config.AssociatedData.Length > 0)
             {
                 dst.Append(",data=");
-                dst.Append(Convert.ToBase64String(config.AssociatedData));
+                dst.Append(config.AssociatedData.ToB64String());
             }
 
             if (config.Salt == null || config.Salt.Length == 0)
@@ -90,7 +104,7 @@ namespace Isopoh.Cryptography.Argon2
             }
 
             dst.Append("$");
-            dst.Append(Convert.ToBase64String(config.Salt));
+            dst.Append(config.Salt.ToB64String());
 
             if (hash == null || hash.Length == 0)
             {
@@ -98,8 +112,51 @@ namespace Isopoh.Cryptography.Argon2
             }
 
             dst.Append("$");
-            dst.Append(Convert.ToBase64String(hash));
+            dst.Append(hash.ToB64String());
             return dst.ToString();
+        }
+
+        /// <summary>
+        /// Make an Argon2 B64 string which is an RFC 4648 Base64 string without the trailing '=' padding.
+        /// </summary>
+        /// <param name="buf">The buffer to convert to a string.</param>
+        /// <returns>The Argon2 B64 string.</returns>
+        public static string ToB64String(this byte[] buf)
+        {
+            var lengthMod3 = buf.Length % 3;
+            var chunkCount = buf.Length / 3;
+            var bufFullChunkLength = chunkCount * 3;
+            var b64len = (chunkCount * 4) + B64Extra[lengthMod3];
+            var ret = new StringBuilder(b64len);
+            int i = 0;
+            for (; i < bufFullChunkLength; ++i)
+            {
+                int c1 = buf[i];
+                int c2 = buf[++i];
+                int c3 = buf[++i];
+                ret.Append(b64Chars[(c1 & 0xFC) >> 2]);
+                ret.Append(b64Chars[((c1 & 0x03) << 4) | ((c2 & 0xF0) >> 4)]);
+                ret.Append(b64Chars[((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6)]);
+                ret.Append(b64Chars[c3 & 0x3F]);
+            }
+
+            switch (lengthMod3)
+            {
+                case 2:
+                    int c1 = buf[i];
+                    int c2 = buf[++i];
+                    ret.Append(b64Chars[(c1 & 0xFC) >> 2]);
+                    ret.Append(b64Chars[((c1 & 0x03) << 4) | ((c2 & 0xF0) >> 4)]);
+                    ret.Append(b64Chars[(c2 & 0xF) << 2]);
+                    break;
+                case 1:
+                    c1 = buf[i];
+                    ret.Append(b64Chars[(c1 & 0xFC) >> 2]);
+                    ret.Append(b64Chars[(c1 & 0x03) << 4]);
+                    break;
+            }
+
+            return ret.ToString();
         }
     }
 }
