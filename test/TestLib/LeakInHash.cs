6d40d361f2e6ea25609bf20c0203966de0713dcc
Michael Heyman <mheyman@gmail.com>
2024-06-23 15:27:36 -0400

    chore: cleaner README example code

--- test/TestLib/LeakInHash.cs
+++ test/TestLib/LeakInHash.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="LeakInVerify.cs" company="Isopoh">
+﻿// <copyright file="LeakInHash.cs" company="Isopoh">
 // To the extent possible under law, the author(s) have dedicated all copyright
 // and related and neighboring rights to this software to the public domain
 // worldwide. This software is distributed without any warranty.
@@ -6,8 +6,7 @@
 
 namespace TestLib;
 
-using System.Collections.Generic;
-using System.Linq;
+using System.Text;
 using Isopoh.Cryptography.Argon2;
 using Isopoh.Cryptography.SecureArray;
 using Xunit.Abstractions;
@@ -15,7 +14,7 @@ using Xunit.Abstractions;
 /// <summary>
 /// Look for leaks.
 /// </summary>
-public static class LeakInVerify
+public static class LeakInHash
 {
     /// <summary>
     /// Look for leaks.
@@ -72,26 +71,35 @@ public static class LeakInVerify
             },
             $"Wrapped {SecureArray.DefaultCall.Os}");
 
-        const string hashString = "$argon2i$v=19$m=65536,t=3,p=1$M2f6+jnVc4dyL3BfMQRzoA==$jO/fOrgqxX90XDVhiYZgIVJJcw0lzIXtRFRCEggXYV8=";
         const string password = "b";
         const int maxIteration = 10;
         var memoryDiff = new long[maxIteration];
+        var config = new Argon2Config
+        {
+            Password = Encoding.UTF8.GetBytes(password),
+            SecureArrayCall = secureArrayCall,
+        };
         for (var i = 0; i < maxIteration; i++)
         {
-            output.WriteLine($"TestVerifyLeaks: Iteration {i + 1} of {maxIteration}");
+            output.WriteLine($"TestHashLeaks: Iteration {i + 1} of {maxIteration}");
             Thread.Sleep(100);
             long prevTotalMemory = GC.GetTotalMemory(true);
-            Argon2.Verify(hashString, password, secureArrayCall);
+            {
+                Argon2.Hash(config);
+            }
+
             Thread.Sleep(100);
             long postTotalMemory = GC.GetTotalMemory(true);
             memoryDiff[i] = postTotalMemory - prevTotalMemory;
         }
 
-        string? failedLockMessage = null;
+        string? failedLocksMessage = null;
         if (failedLocks.Count > 0)
         {
             string s = failedLocks.Count > 1 ? "s" : string.Empty;
-            failedLockMessage = $"{failedLocks.Count} / {lockCount} failed lock{s}, size{s}=[{string.Join(", ", failedLocks.Select(x => $"0x{x.Item1.ToInt64():x8}:{x.Item2.ToUInt64()}"))}].";
+
+            // only printed if any other failures.
+            failedLocksMessage = $"{failedLocks.Count} / {lockCount} failed lock{s}, size{s}=[{string.Join(", ", failedLocks.Select(x => $"0x{x.Item1.ToInt64():x8}:{x.Item2.ToUInt64()}"))}].";
         }
 
         var errs = new List<string>();
@@ -115,6 +123,6 @@ public static class LeakInVerify
             errs.Add($"Leaked {locks.Count} / {lockCount} locks: address:size:index=[{string.Join(", ", locks.Select(kv => $"0x{kv.Key.ToInt64():x8}:{kv.Value.Item1.ToUInt64()}:{kv.Value.Item2}"))}].");
         }
 
-        return (!errs.Any(), errs.Any() ? $"Verify leaks: FAILED: {string.Join(" ", errs)}{(failedLockMessage == null ? string.Empty : $" {failedLockMessage}")}" : "Leaks: Passed");
+        return (!errs.Any(), errs.Any() ? $"Hash leaks: FAILED: {string.Join(" ", errs)}{(failedLocksMessage == null ? string.Empty : $" {failedLocksMessage}")}" : "Leaks: Passed");
     }
 }
---
+++ test/TestLib/MemoryNoAlloc.cs
@@ -0,0 +1,66 @@
+﻿// <copyright file="MemoryNoAlloc.cs" company="Isopoh">
+// To the extent possible under law, the author(s) have dedicated all copyright
+// and related and neighboring rights to this software to the public domain
+// worldwide. This software is distributed without any warranty.
+// </copyright>
+
+namespace TestLib;
+
+using System.Text;
+using Isopoh.Cryptography.Argon2;
+using Isopoh.Cryptography.SecureArray;
+using Xunit.Abstractions;
+
+/// <summary>
+/// Look for leaks.
+/// </summary>
+public static class MemoryNoAlloc
+{
+    /// <summary>
+    /// Look for leaks.
+    /// </summary>
+    /// <param name="output">Used to write output.</param>
+    /// <returns>String with pass/fail message.</returns>
+    public static (bool Passed, string Message) Test(ITestOutputHelper output)
+    {
+        var countLock = new object();
+        var lockCount = 0;
+        var secureArrayCall = new SecureArrayCall(
+            SecureArray.DefaultCall.ZeroMemory,
+            (m, l) =>
+            {
+                string? ret = SecureArray.DefaultCall.LockMemory(m, l);
+                lock (countLock)
+                {
+                    ++lockCount;
+                }
+
+                return ret;
+            },
+            (m, l) =>
+            {
+                SecureArray.DefaultCall.UnlockMemory(m, l);
+            },
+            $"Wrapped {SecureArray.DefaultCall.Os}");
+
+        const string password = "b";
+        var config = new Argon2Config
+        {
+            Password = Encoding.UTF8.GetBytes(password),
+            SecureArrayCall = secureArrayCall,
+        };
+        const int maxIteration = 10;
+        var memory = new Argon2Memory(config, Argon2MemoryPolicy.NoShrink, LockMemoryPolicy.BestEffort);
+        Argon2.Hash(memory);
+        int firstLockCount = lockCount;
+        for (var i = 0; i < maxIteration; i++)
+        {
+            output.WriteLine($"TestMemoryNoAlloc: Iteration {i + 1} of {maxIteration}");
+            Argon2.Hash(memory);
+        }
+
+        return lockCount > firstLockCount
+            ? (false, $"Memory No Alloc: FAILED: Got {lockCount - firstLockCount} allocations in {maxIteration} iterations")
+            : (true, "Memory No Alloc: Passed");
+    }
+}
